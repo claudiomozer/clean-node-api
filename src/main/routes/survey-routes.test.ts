09d83150b0450884799b05b7ed904ac944f1273b
Cláudio André Mozer <claudinhoandre@gmail.com>
2022-01-27 21:07:30 -0300

    test: ensure loadSurveys route return 403 if no accessToken is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -40,37 +40,45 @@ describe('Login Routes', () => {
         })
         .expect(403)
     })
+
+    test('Should return 204 on add survey with valid accessToken', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'Claudio',
+        email: 'claudinhoandre@gmail.com',
+        password: '123',
+        role: 'admin'
+      })
+      const id = res.insertedId
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({
+        _id: id
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'Question',
+          answers: [{
+            image: 'http://image-name.com',
+            answer: 'Answer 1'
+          },
+          {
+            answer: 'Answer 2'
+          }]
+        })
+        .expect(204)
+    })
   })
 
-  test('Should return 204 on add survey with valid accessToken', async () => {
-    const res = await accountCollection.insertOne({
-      name: 'Claudio',
-      email: 'claudinhoandre@gmail.com',
-      password: '123',
-      role: 'admin'
+  describe('GET /surveys', () => {
+    test('Should return 403 on load surveys without accessToken', async () => {
+      await request(app)
+        .get('/api/surveys')
+        .expect(403)
     })
-    const id = res.insertedId
-    const accessToken = sign({ id }, env.jwtSecret)
-    await accountCollection.updateOne({
-      _id: id
-    }, {
-      $set: {
-        accessToken
-      }
-    })
-    await request(app)
-      .post('/api/surveys')
-      .set('x-access-token', accessToken)
-      .send({
-        question: 'Question',
-        answers: [{
-          image: 'http://image-name.com',
-          answer: 'Answer 1'
-        },
-        {
-          answer: 'Answer 2'
-        }]
-      })
-      .expect(204)
   })
 })
