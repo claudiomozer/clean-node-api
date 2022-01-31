d2711b1d415aca4957a1fd4df7f4475086f193e2
Cláudio André Mozer <claudinhoandre@gmail.com>
2022-01-31 20:57:55 -0300

    feat: ensure save survey result route returns 200 on success

--- src/main/routes/survey-result-routes.ts
+++ src/main/routes/survey-result-routes.ts
@@ -1,8 +1,8 @@
 import { adaptRoute } from '@/main/adapters/express-route-adapter'
-import { makeLoadSurveysController } from '@/main/factories/controllers/survey/load-surveys/load-surveys-controller-factory'
 import { auth } from '@/main/middlewares/auth'
+import { makeSaveSurveyResultController } from '../factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory'
 import { Router } from 'express'
 
 export default (router: Router): void => {
-  router.put('/surveys/:surveyId/results', auth, adaptRoute(makeLoadSurveysController()))
+  router.put('/surveys/:surveyId/results', auth, adaptRoute(makeSaveSurveyResultController()))
 }
