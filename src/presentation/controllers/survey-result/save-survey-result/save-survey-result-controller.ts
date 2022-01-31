34853ee1143005e6d3d749273c6a7b0c136b7506
Cláudio André Mozer <claudinhoandre@gmail.com>
2022-01-30 21:40:29 -0300

    feat: ensure SaveSurveyResultController returns 200 on success

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -5,7 +5,7 @@ import {
   LoadSurveyById,
   SaveSurveyResult
 } from './save-survey-result-controller-protocols'
-import { noContent, forbidden, serverError } from '@/presentation/helpers/http/http-helpers'
+import { ok, forbidden, serverError } from '@/presentation/helpers/http/http-helpers'
 import { InvalidParamError } from '@/presentation/errors'
 
 export class SaveSurveyResultController implements Controller {
@@ -28,13 +28,13 @@ export class SaveSurveyResultController implements Controller {
       } else {
         return forbidden(new InvalidParamError('surveyId'))
       }
-      await this.saveSurveyResult.save({
+      const surveyResult = await this.saveSurveyResult.save({
         accountId: accountId ?? '',
         surveyId,
         answer,
         date: new Date()
       })
-      return noContent()
+      return ok(surveyResult)
     } catch (error) {
       return serverError(error)
     }
